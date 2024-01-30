bd18652fb9dda1b8a8052b1fc5d701f78fb146eb
Владимир Матасов <dragon111@mail.ru>
2024-01-30 10:25:40 +0300

    Add first iteration viewsets for "Group" and "Follow"

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,63 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import (
+    BasePermission,
+    IsAuthenticated,
+    SAFE_METHODS
+)
+
+from posts.models import Group, Post
+from .serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer
+)
+
+
+class IsPostOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsPostOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsPostOrReadOnly)
+
+    def get_post(self):
+        post = get_object_or_404(
+            Post,
+            id=self.kwargs.get('post_id')
+        )
+        return post
+
+    def get_queryset(self):
+        post = self.get_post()
+        return post.comments
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self.get_post()
+        )
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    # permission_classes = (IsAuthenticated, IsPostOrReadOnly)
